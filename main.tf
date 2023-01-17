f0f1e99dea5f1b444abaaf5098c58229d2e8ab60
vvnair-amzn <vvnair@amazon.com>
2023-01-17 15:26:25 +0530

    Issue fix : https://github.com/aws-samples/aws-codepipeline-terraform-cicd-samples/issues/8

--- main.tf
+++ main.tf
@@ -90,6 +90,7 @@ module "codepipeline_kms" {
 module "codepipeline_iam_role" {
   source                     = "./modules/iam-role"
   project_name               = var.project_name
+  create_new_role            = var.create_new_role
   codepipeline_iam_role_name = var.create_new_role == true ? "${var.project_name}-codepipeline-role" : var.codepipeline_iam_role_name
   source_repository_name     = var.source_repo_name
   kms_key_arn                = module.codepipeline_kms.arn
