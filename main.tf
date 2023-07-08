5814ca97cd202b9d9b727cf3a2aa239f553cb4f5
Laura Pena <laura.pena@global.ntt>
2023-07-08 02:53:00 +0200

    remove codecomit rules, add new pipeline

--- main.tf
+++ main.tf
@@ -39,8 +39,9 @@ module "codecommit_infrastructure_source_repo" {
   create_new_repo          = var.create_new_repo
   source_repository_name   = var.source_repo_name
   source_repository_branch = var.source_repo_branch
-  repo_approvers_arn       = var.repo_approvers_arn
-  kms_key_arn              = module.codepipeline_kms.arn
+  source_repository_description = var.source_repo_description
+  ## repo_approvers_arn       = var.repo_approvers_arn
+  ## kms_key_arn              = module.codepipeline_kms.arn
   tags = {
     Project_Name = var.project_name
     Environment  = var.environment
@@ -102,6 +103,7 @@ module "codepipeline_iam_role" {
     Region       = local.region
   }
 }
+
 # Module for Infrastructure Validate, Plan, Apply and Destroy - CodePipeline
 module "codepipeline_terraform" {
   depends_on = [
@@ -116,6 +118,29 @@ module "codepipeline_terraform" {
   s3_bucket_name        = module.s3_artifacts_bucket.bucket
   codepipeline_role_arn = module.codepipeline_iam_role.role_arn
   stages                = var.stage_input
+  kms_key_arn           = module.codepipeline_kms.arn
+  tags = {
+    Project_Name = var.project_name
+    Environment  = var.environment
+    Account_ID   = local.account_id
+    Region       = local.region
+  }
+}
+
+# Module for Infrastructure Validate, Plan, Apply and Destroy - CodePipeline
+module "codepipeline_terraform_destroy" {
+  depends_on = [
+    module.codebuild_terraform,
+    module.s3_artifacts_bucket
+  ]
+  source = "./modules/codepipeline"
+
+  project_name          = "${var.project_name}-destroy"
+  source_repo_name      = var.source_repo_name
+  source_repo_branch    = var.source_repo_branch
+  s3_bucket_name        = module.s3_artifacts_bucket.bucket
+  codepipeline_role_arn = module.codepipeline_iam_role.role_arn
+  stages                = var.stage_destroy_input
   kms_key_arn           = module.codepipeline_kms.arn
   tags = {
     Project_Name = var.project_name
